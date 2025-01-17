65aac05b40734101f9f836f390d1b58c2ecb93c0
Ardeky <gustavo.ruizcorrea@gmail.com>
2025-01-17 05:27:49 -0500

    config musicFile

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,7 +1,7 @@
 import { useGameOver } from '../utils/gameHelpers';
 import Tetris from './Tetris';
 import Menu from './Menu';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import musicFile from '../assets/Music/menuIntro.mp3';
 
 interface GameProps {
@@ -11,24 +11,26 @@ interface GameProps {
 
 const Game: React.FC<GameProps> = ({ rows, columns }) => {
   const [gameOver, setGameOver, resetGameOver] = useGameOver();
+  const audioRef = useRef<HTMLAudioElement | null>(null); 
   
-  useEffect(() => { 
-    const audio = new Audio(musicFile);
-
+  useEffect(() => {
+    audioRef.current = new Audio(musicFile);
+    audioRef.current.loop = true; 
+    audioRef.current.play();
+    
     return () => {
-      if (audio) {
-      audio.pause(); 
-      audio.currentTime = 0;
+      if (audioRef.current) {
+        audioRef.current.pause();
+        audioRef.current.currentTime = 0;
       } 
     }; 
-  }, []);
-
+  }, []); 
+  
   const start = () => {
-    const audio = new Audio(musicFile);
-    resetGameOver();
-    audio.loop = true;
-    audio.play();
-    
+    resetGameOver(); 
+    if (audioRef.current) {
+      audioRef.current.play(); 
+    } 
   };
 
   return (
